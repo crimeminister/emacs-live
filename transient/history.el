4370802e1bf5d3719eb7693ae90ca5cc26258ac7
Robert Medeiros <robert@crimeminister.org>
2020-06-22 13:08:23 -0400

    Update transient history

--- transient/history.el
+++ transient/history.el
@@ -1,1 +1,19 @@
-nil
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  (("--" "setup.yaml"))
+  (("--" "setup.yaml")
+   "--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch
+  ("--prune")
+  ("--prune" "--tags"))
+ (magit-gitignore nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
+ (magit-push nil
+             ("--force-with-lease"))
+ (magit-stash nil)
+ (magit:-- "setup.yaml" ""))
